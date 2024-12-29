fd9c93943173cd8829eaff825eb6378074876019
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-12-29 21:35:10 +0000

    operator events
    TODO: slash states

--- x/operator/keeper/opt.go
+++ x/operator/keeper/opt.go
@@ -76,6 +76,15 @@ func (k *Keeper) OptIn(
 	if err != nil {
 		return err
 	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypeOptIn,
+			sdk.NewAttribute(types.AttributeKeyOperator, operatorAddress.String()),
+			sdk.NewAttribute(types.AttributeKeyAVSAddr, avsAddr),
+		),
+	)
+
 	return nil
 }
 
